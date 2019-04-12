16b037c2a1a819a29472bdf9114fdbbf38830837
Ryan <ryannitz@amazon.com>
2019-04-12 14:11:43 -0400

    Dev

--- redirect/app.py
+++ redirect/app.py
@@ -1,10 +1,15 @@
 import json
 
 def handler(event, context):
+    ip = event['requestContext']['identity']['sourceIp']
+    agent = event['requestContext']['identity']['userAgent']
+
+    print('ip: {} - agent: {}'.format(ip, agent))
+
     return {
         'isBase64Encoded': False,
         'headers': {},
-        'statusCode': 200,
+        'statusCode': 302,
         'body': json.dumps({
             'location': 'https://aws.amazon.com',
         }),
